360f111b5d4c19c775271a011f10ffe5a0d62084
Cole <cebCole@gmail.com>
2022-12-14 07:19:41 -0600

    basic setup, currently broken

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
+        <h1>Hello World</h1>
         <App />
     </React.StrictMode>
 );
