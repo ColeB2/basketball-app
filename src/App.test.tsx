edebd58ad53420440ec8d81fb34d7ad414ddf03d
Cole <cebCole@gmail.com>
2023-04-04 14:56:18 -0500

    basic table testing work, not complete

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,5 +1,8 @@
-import { describe } from 'vitest';
-import { render, screen, waitFor } from '@testing-library/react';
+import { describe, expectTypeOf } from 'vitest';
+import { fireEvent, render, screen, waitFor } from '@testing-library/react';
+
+import todayGameData from './tests/todayGameData';
+import yesterdayGameData from './tests/yesterdayGameData';
 
 import App from './App';
 
@@ -7,7 +10,7 @@ describe('App', () => {
     it('Renders App component', async () => {
         render(<App />);
 
-        await waitFor(() => screen.getByText('CLE'));
+        await screen.findAllByRole('table');
         const tables = screen.getAllByRole('table');
         // After loading we should have 15 tables;
         // 4 today games, 1 date table/ 10 tomorrow games.
@@ -16,24 +19,91 @@ describe('App', () => {
     it('Renders Tables/Games Properly', async () => {
         render(<App />);
 
-        await waitFor(() => screen.getByText('CLE'));
+        await screen.findAllByRole('table');
+
         const tables = screen.getAllByRole('table');
         // After loading we should have 15 tables;
         // 4 today games, 1 date table/ 10 tomorrow games.
         expect(tables.length).toBe(15);
 
-        for (let i = 0; i <= 3; i++) {
+        for (let i = 0; i <= tableDataToday.length - 1; i++) {
             const table = tables[i];
-            expect(table).toHaveTextContent(tableDataToday[i]);
+            // //Below DOESNT WORK FIX>
+            // const [awayTeamName, awayAbbr, awayScore] =
+            //     table.querySelectorAll('tr:nth-child(2) td');
+            // const [homeTeamName, homeAbbr, homeScore] =
+            //     table.querySelectorAll('tr:nth-child(3) td');
+
+            // expect(awayTeamName).toHaveTextContent(
+            //     yesterdayGameData.data[i].visitor_team.full_name
+            // );
+            // expect(awayAbbr).toHaveTextContent(
+            //     yesterdayGameData.data[i].visitor_team.abbreviation
+            // );
+            // expect(awayScore).toHaveTextContent(
+            //     yesterdayGameData.data[i].visitor_team_score.toString()
+            // );
+
+            // expect(homeTeamName).toHaveTextContent(
+            //     yesterdayGameData.data[i].home_team.full_name
+            // );
+            // expect(homeAbbr).toHaveTextContent(
+            //     yesterdayGameData.data[i].home_team.abbreviation
+            // );
+            // expect(homeScore).toHaveTextContent(
+            //     yesterdayGameData.data[i].home_team_score.toString()
+            // );
+            // Assert first row of each table is Status --> Final
+            const rows = table.querySelectorAll('tr');
+            expect(rows[0].textContent).toBe('Final');
+            const awayCells = rows[1].querySelectorAll('td');
+            const [awayTeamName, awayAbbr, awayScore] = awayCells;
+            expect(awayTeamName.textContent).toBe(
+                yesterdayGameData.data[i].visitor_team.full_name
+            );
+            expect(awayAbbr.textContent).toBe(
+                yesterdayGameData.data[i].visitor_team.abbreviation
+            );
+            expect(awayScore.textContent).toBe(
+                yesterdayGameData.data[i].visitor_team_score.toString()
+            );
+            const homeCells = rows[2].querySelectorAll('td');
+            const [homeTeamName, homeAbbr, homeScore] = homeCells;
+            expect(homeTeamName.textContent).toBe(
+                yesterdayGameData.data[i].home_team.full_name
+            );
+            expect(homeAbbr.textContent).toBe(
+                yesterdayGameData.data[i].home_team.abbreviation
+            );
+            expect(homeScore.textContent).toBe(
+                yesterdayGameData.data[i].home_team_score.toString()
+            );
         }
-        for (let i = 0; i < tables.length; i++) {
-            const table = tables[i];
-            console.log(table.textContent);
-        }
-        for (let i = 5; i < tables.length; i++) {
-            const table = tables[i];
-            expect(table).toHaveTextContent(tableDataTomo[i - 5]);
-        }
+    });
+    it('Renders Boxscore when clicking today game table.', async () => {
+        render(<App />);
+
+        await waitFor(() => screen.getByText('CLE'));
+        const tables = screen.getAllByRole('table');
+        // After loading we should have 15 tables;
+        // 4 today games, 1 date table/ 10 tomorrow games.
+        fireEvent.click(tables[0]);
+        // Click on tables and wait for the other boxscores tables to load.
+        await waitFor(() =>
+            expect(screen.getAllByRole('table').length).toBeGreaterThan(
+                tables.length
+            )
+        );
+        const tables2 = screen.getAllByRole('table');
+        // console.log(tables.length, tables2.length);
+        // console.log(tables2[tables2.length - 1].textContent);
+        // const boxscore = screen.getByText('Garland');
+        // expect(tables.length).toBe(15);
+
+        // for (let i = 0; i <= tableDataToday.length - 1; i++) {
+        //     const table = tables[i];
+        //     expect(table).toHaveTextContent(tableDataToday[i]);
+        // }
     });
 });
 
--- src/components/DateCard/DateCard.tsx
+++ src/components/DateCard/DateCard.tsx
@@ -1,5 +1,3 @@
-import React from 'react';
-
 import { basketballData } from '../../types/basketballdata';
 import './DateCard.css';
 
