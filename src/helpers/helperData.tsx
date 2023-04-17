b0226f76a51a6d75a7093eaf4143540496e1c81a
Cole <cebCole@gmail.com>
2023-04-17 12:44:01 -0500

    forgotton helper data file

---
+++ src/helpers/helperData.tsx
@@ -0,0 +1,14 @@
+export const emptyDateObject = {
+    dateObj: true,
+    date: null,
+    home_team: {},
+    home_team_score: 0,
+    id: 0,
+    period: 0,
+    postseason: 0,
+    season: 0,
+    status: 0,
+    time: 0,
+    visitor_team: 0,
+    visitor_team_score: 0,
+};
