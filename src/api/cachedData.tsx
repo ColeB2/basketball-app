3b0ab052bc31dc98919a9f9abc7b1d84f1225bf5
Cole <cebCole@gmail.com>
2023-05-02 13:11:25 -0500

    data caches

---
+++ src/api/cachedData.tsx
@@ -0,0 +1,7 @@
+import {
+    cachedBasketballDataType,
+    cachedBoxscoreDataType,
+} from '../types/basketballdata';
+
+export const cachedGameData = {} as cachedBasketballDataType;
+export const cachedBoxscoreData = {} as cachedBoxscoreDataType;
